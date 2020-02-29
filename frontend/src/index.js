debcfc9be532337dd80e0207b33475bb11653c7a
Tigran Kirakosov <tigran_kirakosov@epam.com>
2020-02-29 21:59:59 +0300

    Test commit for PR

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "regenerator-runtime";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import App from "./components/App/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import 'regenerator-runtime';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import App from './components/App/App';
 
-const appRoot = document.getElementById("react-root");
+const appRoot = document.getElementById('react-root');
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    appRoot
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  appRoot
 );
